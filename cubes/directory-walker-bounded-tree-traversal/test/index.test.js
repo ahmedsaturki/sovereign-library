01839966842b76e3acec7a41e742d289b55570d7
ahmedsaturki <ahmedsaeedturki@gmail.com>
2026-08-25 23:31:56 +0300

    fix: import node test runner in directory walker suite

--- cubes/directory-walker-bounded-tree-traversal/test/index.test.js
+++ cubes/directory-walker-bounded-tree-traversal/test/index.test.js
@@ -1,3 +1,4 @@
+import { test } from 'node:test';
 import assert from 'node:assert/strict';
 import { mkdtemp, mkdir, symlink, writeFile, rm } from 'node:fs/promises';
 import { join } from 'node:path';
